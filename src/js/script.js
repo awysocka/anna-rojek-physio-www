ec68c4acd5506d2b06cc42a00c46185dfc2c8a04
awysocka <wysocka.anna88@gmail.com>
2019-08-08 22:10:56 +0200

    change images quality

--- src/js/script.js
+++ src/js/script.js
@@ -88,3 +88,13 @@ for(let cityButton of cityButtons) {
     }
   };
 }
+
+// --- IRFRAME ---
+
+function init() {
+  var vidDefer = document.getElementsByTagName('iframe');
+  for (var i=0; i<vidDefer.length; i++) {
+  if(vidDefer[i].getAttribute('data-src')) {
+  vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+  } } }
+  window.onload = init;
